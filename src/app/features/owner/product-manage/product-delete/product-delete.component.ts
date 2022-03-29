d8bba443c118da271d82f039e311fb88cea1fd70
tomás alfonso <tomas.alfonso@outlook.com.ar>
2022-03-29 17:02:07 -0600

    me and vadell made the communication between  delete component and list

--- src/app/features/owner/product-manage/product-delete/product-delete.component.ts
+++ src/app/features/owner/product-manage/product-delete/product-delete.component.ts
@@ -1,9 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { IProduct } from 'src/app/core/entities';
-import { ProductService } from 'src/app/shared/services/product/product.service';
 
 @Component({
 	selector: 'app-product-delete',
@@ -12,65 +9,16 @@ import { ProductService } from 'src/app/shared/services/product/product.service'
 })
 export class ProductDeleteComponent implements OnInit {
 
-	public productForm: FormGroup;	
-
 	constructor(
-		private productService: ProductService,
-		private snackBar: MatSnackBar,
 		public dialogRef: MatDialogRef<ProductDeleteComponent>,
     	@Inject(MAT_DIALOG_DATA) public data: IProduct,
 	) { }
 
 	ngOnInit(): void {
-		this.initForm();
 	}
 
-	public initForm(): void {
-		this.productForm = new FormGroup({
-			'name': new FormControl({value: this.data.name, disabled: true}),
-			'cost': new FormControl({value: this.data.cost, disabled: true}),
-            'salePrice': new FormControl({value: this.data.salePrice, disabled: true}),
-            'listPrice': new FormControl({value: this.data.listPrice, disabled: true}),
-            'stockSize': new FormControl({value: this.data.stockSize, disabled: true}),
-		})
-	}
-
-	public delete(): void {
-		this.productService.getById(this.data.id).get().subscribe(element => {
-
-			let product: IProduct = {
-				id: this.data.id,
-				name: this.productForm.get('name')?.value,
-				cost: this.productForm.get('cost')?.value,
-				salePrice: this.productForm.get('salePrice')?.value,
-				listPrice: this.productForm.get('listPrice')?.value,
-				stockSize: this.productForm.get('stockSize')?.value,
-				img: this.productForm.get('img')?.value,
-				firebaseTimestamp: Date.now(),
-				firebaseId: element.docs[0].id,
-				disabled: false,
-			}
-
-			this.productService.delete(product)
-				.then(() => {
-					this.snackBar.open("Producto Eliminado", "Cerrar", {
-						duration: 3000,
-						panelClass: ['orange-snackbar']
-					});
-				})
-				.catch( error => {
-					console.log("line 63 - product-add", error);
-					this.snackBar.open("Error al intentar eliminar el producto", "Cerrar", {
-						duration: 3000,
-						panelClass: ['red-snackbar']
-					});
-				})
-				.finally(() => {
-					this.dialogRef.close();
-				})
-
-		})
-
+	public closeDialog($event: any): void {
+		this.dialogRef.close();
 	}
 
 }
