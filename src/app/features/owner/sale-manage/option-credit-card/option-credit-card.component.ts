9f663a22c51c27467e7387d4cfecc1882a5ebcc2
Yamil Foglia <yamil.foglia@gmail.com>
2022-04-01 18:05:03 -0300

    fix visual

--- src/app/features/owner/sale-manage/option-credit-card/option-credit-card.component.ts
+++ src/app/features/owner/sale-manage/option-credit-card/option-credit-card.component.ts
@@ -10,11 +10,11 @@ export class OptionCreditCardComponent implements OnInit {
     public price = 10000;
     public total = 0;
     
-    public fee3: any = (this.price / 3).toFixed();
-    public fee6: any = ((this.price + ((this.price * 9) / 100)) / 6).toFixed();
-    public fee9: any = ((this.price + ((this.price * 30) / 100)) / 9).toFixed();
-    public fee12: any = (this.price / 12).toFixed();
-    public fee18: any = ((this.price + ((this.price * 10) / 100)) / 18).toFixed();
+    public fee3: any = (this.price / 3).toFixed(2);
+    public fee6: any = ((this.price + ((this.price * 9) / 100)) / 6).toFixed(2);
+    public fee9: any = ((this.price + ((this.price * 30) / 100)) / 9).toFixed(2);
+    public fee12: any = (this.price / 12).toFixed(2);
+    public fee18: any = ((this.price + ((this.price * 10) / 100)) / 18).toFixed(2);
 
     public total3: any = (this.fee3 * 3).toFixed();
     public total6: any = (this.fee6 * 6).toFixed();
